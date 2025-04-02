cf0e14bc85c1a673c886946f4446857ff939d749
Renan F <renanfrancisco.on@gmail.com>
2025-04-02 20:54:16 -0300

    [add]eslint + prettier + primeido servidor local na vida

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,16 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js"
+import prettier from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+export default [
+  js.configs.recommended,
+  prettier,
+  prettierConfig,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+      "prettier/prettier": "error", // Mostra erro se Prettier não for seguido
+      "no-console": "warn", // Exibe aviso para console.log()
+      "no-unused-vars": "warn" // Exibe aviso para variáveis não usadas
+    }
+  }
+]
