edc5a1a2e0d741b1a0e40ff415dafb9688944681
Renan F <renanfrancisco.on@gmail.com>
2025-04-03 21:56:48 -0300

    criei um post e um provider

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    @Get()
-    async getAllUsers() {
-        return JSON.stringify({ test: 'abc' });
+    constructor(private readonly userService: UserService) {
+
     }
+
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto) {
+        return this.userService.createUser(createUser);
+    }
+    // o de baixo foi pra testar
+    // @Get()
+    // async getAllUsers() {
+    //     return JSON.stringify({ test: 'abc' });
+    // }
 }
